037a30ecbd8ddc21b87240989b56814e090e673d
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-10-02 20:19:40 +0000

    unit tests requested

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-// This is my function that sums two numbers
-const sum = (a,b) => {
-    return a + b
+
+// One euro is:
+let oneEuroIs = {
+    "JPY": 156.5, // japan yen
+    "USD": 1.07, // us dollar
+    "GBP": 0.87, // british pound
 }
 
-// Just a console log for ourselves
-console.log(sum(7,3))
+function fromDollarToYen (dollar) {
+    let yen = Math.round(dollar * (oneEuroIs.JPY/oneEuroIs.USD));
+    return yen;
+}
 
-// Export the function to be used on other files 
-// (similar to the keyword "export" when using webpack)
-module.exports = { sum };
+function fromEuroToDollar (euro) {
+    let dollar = Math.round(euro * oneEuroIs.USD);
+    return dollar;
+}
+
+function fromYenToPound (yen) {
+    let pound = Math.round(yen * (oneEuroIs.GBP/oneEuroIs.JPY));
+    return pound;
+}
+
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound, }
--- test.js
+++ test.js
@@ -1,11 +1,79 @@
-// Import the function sum from the app.js file
-const { sum } = require('./app.js');
+test("1 euro should be 1.07 dollars", function() {
+    // Import the function from app.js
+    const { fromEuroToDollar } = require('./app.js');
 
-// Start your first test
-test('adds 14 + 9 to equal 23', () => {
-    // Inside the test we call our sum function with 2 numbers
-    let total = sum(14, 9);
+    // Use the function like its supposed to be used
+    const dollars = fromEuroToDollar(3.5);
 
-    // We expect the sum of those 2 numbers to be 23
-    expect(total).toBe(23);
-});
+    // If 1 euro is 1.07 dollars, then 3.5 euros should be (3.5 * 1.07)
+    const expected = Math.round(3.5 * 1.07); 
+    
+    // This is the comparison for the unit test
+     expect(dollars).toBe(expected); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
+})
+
+test("1 dollar should be 146.262 yen", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+    //1 dollar should be 146.262 yen
+    const expected = Math.round(1 * 146.262); 
+    expect(fromDollarToYen(1)).toBe(expected); 
+    expect(fromDollarToYen(1)).not.toBe(NaN); 
+    expect(fromDollarToYen(1)).not.toBe(undefined); 
+    expect(fromDollarToYen(1)).toBeGreaterThanOrEqual(0);
+})
+
+test("20 dollar should be 2925.234 yen", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+    //1 dollar should be 146.262 yen
+    const expected = Math.round(20 * 146.262); 
+    expect(fromDollarToYen(20)).toBe(expected); 
+    expect(fromDollarToYen(20)).not.toBe(NaN); 
+    expect(fromDollarToYen(20)).not.toBe(undefined); 
+    expect(fromDollarToYen(20)).toBeGreaterThanOrEqual(0);
+})
+
+test("500 dollar should be 73130.841 yen", function() {
+    // Import the function from app.js
+    const { fromDollarToYen } = require('./app.js');
+    //1 dollar should be 146.262 yen    
+    const expected = Math.round(500 * 146.262); 
+    expect(fromDollarToYen(500)).toBe(expected); 
+    expect(fromDollarToYen(500)).not.toBe(NaN); 
+    expect(fromDollarToYen(500)).not.toBe(undefined); 
+    expect(fromDollarToYen(500)).toBeGreaterThanOrEqual(0);
+})
+
+test("1 yen should be 0.006 pound", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+    //1 yen should be 0.006 pound
+    const expected = Math.round(1 * 0.006); 
+    expect(fromYenToPound(1)).toBe(expected); 
+    expect(fromYenToPound(1)).not.toBe(NaN); 
+    expect(fromYenToPound(1)).not.toBe(undefined); 
+    expect(fromYenToPound(1)).toBeGreaterThanOrEqual(0);
+})
+
+test("300 yen should be 1.668 pound", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+    //1 yen should be 0.006 pound
+    const expected = Math.round(300 * 0.006); 
+    expect(fromYenToPound(300)).toBe(expected); 
+    expect(fromYenToPound(300)).not.toBe(NaN); 
+    expect(fromYenToPound(300)).not.toBe(undefined); 
+    expect(fromYenToPound(300)).toBeGreaterThanOrEqual(0);
+})
+
+test("500 yen should be 2.780 pound", function() {
+    // Import the function from app.js
+    const { fromYenToPound } = require('./app.js');
+    //1 yen should be 0.006 pound
+    const expected = Math.round(500 * 0.006); 
+    expect(fromYenToPound(500)).toBe(expected); 
+    expect(fromYenToPound(500)).not.toBe(NaN); 
+    expect(fromYenToPound(500)).not.toBe(undefined); 
+    expect(fromYenToPound(500)).toBeGreaterThanOrEqual(0);
+})
